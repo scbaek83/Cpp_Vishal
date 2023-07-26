28f0be9877568dadbe3dcb301eacd7845bdb0707
scbaek83 <scpenny@hanmail.net>
2023-07-26 16:58:35 +0900

    SRP implementation
    from Vishal tutorial

---
+++ srp_adopted.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <vector> 
+
+using namespace std; 
+
+class Journal {
+    string m_title; 
+    vector<string> m_entries; 
+
+public: 
+    explicit Journal(const string &title) : m_title{title} {} 
+    void add_entries(const string &entry) { 
+        static int32_t count = 1; 
+        m_entries.push_back(to_string(count++) + ": " + entry); 
+    }
+
+    auto get_entries() const { return m_entries; } 
+};
+
+struct SavingManager { 
+    static void save(const Journal &j, const string &filename) {
+        ofstream ofs(filename); 
+        for (auto &s : j.get_entries()) 
+            ofs << s << endl; 
+    }
+};
+
+int main() 
+{
+    Journal journal{"Dear XYZ"}; 
+    journal.add_entries("I ate a bug2"); 
+    journal.add_entries("I cried today2"); 
+    
+    SavingManager::save(journal, "diary.txt"); 
+
+    return EXIT_SUCCESS;
+}
---
+++ srp_violation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string> 
+#include <vector> 
+#include <fstream>
+
+using namespace std; 
+
+class Journal {
+    string m_title; 
+    vector<string> m_entries;  
+
+public: 
+    explicit Journal(const string &title) : m_title{title} {} 
+    void add_entries(const string &entry) {
+        static uint32_t count = 1; 
+        m_entries.push_back(to_string(count++) + ": " + entry); 
+    }
+    auto get_entries() const { return m_entries; } 
+    void save(const string &filename) {
+        ofstream ofs(filename); 
+        for (auto &s : m_entries) 
+            ofs << s << endl; 
+    }
+};
+
+int main() {
+    Journal journal{"Dear XYZ"}; 
+    journal.add_entries("I ate a bug"); 
+    journal.add_entries("I cried today"); 
+    journal.save("diary.txt"); 
+
+    return EXIT_SUCCESS;
+}
